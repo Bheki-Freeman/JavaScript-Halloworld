47b31b221aebda9a8fbd1d13b831dabee3739098
Bheki-Freeman <64682211+Bheki-Freeman@users.noreply.github.com>
2020-05-02 10:30:31 +0200

    Update Hallo.js

--- Hallo.js
+++ Hallo.js
@@ -4,3 +4,13 @@ for (let i = 0; i< 3; i++){
 }
 
 //This only loops for three times the Hello World Message, and then it also notifies the user as to which position is that loop. 
+
+//After having created My file and gone to sleep, I then want to continue working so here is my code to make progress on my program
+
+let user_Name = "Freeman";
+if (user_Name === "Freeman"){
+  console.log("Yes u are the user indeed");
+}
+else{
+  console.log("No, u are not!");
+}
