2cc8209d99169e948bf9291569806470e10e290f
Bheki-Freeman <64682211+Bheki-Freeman@users.noreply.github.com>
2020-05-02 10:26:06 +0200

    Create Hallo.js

---
+++ Hallo.js
@@ -0,0 +1,6 @@
+let message = "Hellow World, How is JavaScript coding?";
+for (let i = 0; i< 3; i++){
+  console.log(message + " the " + i + " Message. ");
+}
+
+//This only loops for three times the Hello World Message, and then it also notifies the user as to which position is that loop. 
